90c4fd6e2ffa408902de8a6e5fe8c0e5a2c40ad6
Daniil Kurkovskiy <Kurkovskiy.D.P@sberbank.ru>
2021-08-10 12:40:51 +0300

    if else

--- src/Draft.java
+++ src/Draft.java
@@ -1,22 +1,38 @@
 public class Draft {
 
-    public void first(String[] args) {
+//    public static void main(String[] args) {
+//
+//        int X = 69;
+//        X += 5;
+//        X -= 5;
+//
+//        System.out.println(X);
+//    }
 
-        int X = 69;
-        X += 5;
-        X -= 5;
+//    public static void main(String[] args) {
+//        int days = 5000;
+//        int years = days / 365;
+//        int month = (days % 365) / 31;
+//        int leftDays = days - years * 365 - month * 31;
+//        System.out.println("Общее количество дней: " + days);
+//        System.out.println("Лет: " + years);
+//        System.out.println("Месяцев: " + month);
+//        System.out.println("Дней: " + leftDays);
+//    }
 
-        System.out.println(X);
-    }
+//    public static void main(String[] args) {
+//
+//                int temp = 23;
+//                if (temp > 25) {
+//                    System.out.println("Кондиционер включён");
+//                } else if (temp < 22) {
+//                    System.out.println("Кондиционер выключен");
+//                } else {
+//                    System.out.println("Кондиционер ничего не делает");
+//                }
+//    }
 
     public static void main(String[] args) {
-        int days = 5000;
-        int years = days / 365;
-        int month = (days % 365) / 31;
-        int leftDays = days - years * 365 - month * 31;
-        System.out.println("Общее количество дней: " + days);
-        System.out.println("Лет: " + years);
-        System.out.println("Месяцев: " + month);
-        System.out.println("Дней: " + leftDays);
+
     }
 }
