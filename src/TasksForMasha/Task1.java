b24b6e58ac8992a96a681ee092d27da092605953
Daniil Kurkovskiy <Kurkovskiy.D.P@sberbank.ru>
2021-08-11 10:47:49 +0300

    Первая задачка для Маши

---
+++ src/TasksForMasha/Task1.java
@@ -0,0 +1,18 @@
+package TasksForMasha;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите свой вес: ");
+        double weight = in.nextDouble();
+        System.out.print("Введите свой рост: ");
+        double height = in.nextDouble();
+        height = height / 100;
+        height = height * height;
+        String fornatedDouble = new DecimalFormat("#0.0").format(weight / height);
+        System.out.println("Ваш ИМТ = " + fornatedDouble);
+    }
+}
